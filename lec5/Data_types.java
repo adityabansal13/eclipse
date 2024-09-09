ce357e47002b5f5f275985b538a799c3793095e8
adityabansal13 <128966158+adityabansal13@users.noreply.github.com>
2024-09-10 00:14:11 +0530

    Add files via upload

---
+++ lec5/Data_types.java
@@ -0,0 +1,19 @@
+package lec5;
+
+import java.util.Scanner;
+
+public class Data_types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+		
+		byte b = (byte)(420);
+		short s = (short)(125645);
+		int a = 19;
+		long l = 19;
+		System.out.println(s);
+	}
+
+}
