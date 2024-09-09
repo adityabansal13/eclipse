2a525df72401bd4620d368d93d6b549177363ccd
adityabansal13 <128966158+adityabansal13@users.noreply.github.com>
2024-09-10 00:16:25 +0530

    Add files via upload

---
+++ lec8/ProductofArray.java
@@ -0,0 +1,32 @@
+package lec8;
+
+public class ProductofArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4 };
+		int[] num = Array_Product(arr);
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i] + " ");
+		}
+	}
+
+	public static int[] Array_Product(int[] arr) {
+		int n = arr.length;
+		int[] left = new int[n];
+		left[0] = 1;
+		for (int i = 1; i < left.length; i++) {
+			left[i] = left[i - 1] * arr[i - 1];
+		}
+		int[] right = new int[n];
+		right[n - 1] = 1;
+		for (int i = right.length - 2; i >= 0; i--) {
+			right[i] = right[i + 1] * arr[i + 1];
+		}
+		for (int i = 0; i < left.length; i++) {
+			left[i] = left[i] * right[i];
+		}
+		return left;
+	}
+
+}
