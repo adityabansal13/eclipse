014cb74a7b4d119b9f11972c08c0604876de0fed
Aditya Bansal <128966158+adityabansal13@users.noreply.github.com>
2024-10-12 10:07:09 +0530

    Add files via upload

---
+++ lec23/Person_Client.java
@@ -0,0 +1,20 @@
+package lec23;
+
+public class Person_Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		Person p1 = new Person();
+		p1.setName("Kamal");
+		System.out.println(p1.getName());
+		p1.setAge(-12);
+		System.out.println(p1.getAge());
+//		System.out.println(p1.name);
+//		System.out.println(p1.age);		
+//		Person p2 = new Person("Kamlesh",21);
+//		System.out.println(p2.name);
+//		System.out.println(p2.age);
+	}
+
+}
